4da59a41f09b9bc5a6dca77987579e853128246a
Ruth Ahoueya <ruahoueya@gmailcom>
2025-02-17 11:06:43 +0100

    identité sur la page principale

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <p>Hello c&apos;est Amra</p>
         <Image
           className={styles.logo}
           src="/next.svg"
